b8807e9ad039d91eff0ee9bab4ce3d2f95db142a
ttowntom <git.running054@passinbox.com>
2024-10-28 19:04:22 +0100

    fix bug

--- src/utils/getWeekday.js
+++ src/utils/getWeekday.js
@@ -1,5 +1,8 @@
+// Date FNS
+import { format } from "date-fns";
+
 // Resolve today's weekday
 export default function getWeekday(timestamp) {
-	const date = parseTimestamp(timestamp);
+	const date = timestamp;
 	return format(date, "EEEE").toLocaleLowerCase();
 }
