5c6e19f031586a8b26fe36a74ec6f3ee2e817c74
ttowntom <git.running054@passinbox.com>
2024-11-26 17:23:45 +0100

    ensures min-h-svh and adds footer

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function RootLayout() {
   return (
-    <>
+    <div className="flex min-h-svh flex-col">
       <Navbar />
       <main className="flex flex-grow px-4">
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
