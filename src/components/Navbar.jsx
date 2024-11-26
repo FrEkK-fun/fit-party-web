26f3b1ae9e5cbbd3255c0075ddf77495c31f9c9b
ttowntom <git.running054@passinbox.com>
2024-11-26 15:12:29 +0100

    improve navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,43 +2,41 @@ import { useState, useRef, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { byPrefixAndName } from '@awesome.me/kit-43505c22f8/icons';
-import { useAuthContext } from '../hooks/useAuthContext';
+
+import useAuthStore from '../store/authStore';
 import { useLogout } from '../hooks/useLogout';
+
 import MainNavLink from './MainNavLink';
+import Button from './Button';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef(null);
   const { logout } = useLogout();
-  const { user } = useAuthContext();
+  const user = useAuthStore((state) => state.user);
 
   const handleLogout = () => {
     logout();
   };
 
-  const toggleMenu = () => {
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Prevent event propagation
     setIsOpen(!isOpen);
   };
 
-  // Close menu on outside click
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  const handleNavLinkClick = () => {
+    if (window.innerWidth < 640) {
+      setIsOpen(false); // Close menu on small screens
+    }
+  };
 
   // Ensure menu is always open on screens larger than 'sm'
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 640) {
         setIsOpen(true);
+      } else {
+        setIsOpen(false); // Close menu on small screens
       }
     };
 
@@ -53,54 +51,55 @@ const Navbar = () => {
     <>
       {/* Hamburger menu icon */}
       <FontAwesomeIcon
-        icon={byPrefixAndName.fas['bars']}
+        icon={byPrefixAndName.fas[!isOpen ? 'bars' : 'x']}
         onClick={toggleMenu}
-        className="absolute right-4 top-4 text-2xl text-color-system-accent-pink sm:hidden"
+        className="absolute right-4 top-6 text-2xl text-color-system-accent-pink sm:hidden"
       />
 
-      <header className="mb-6 flex justify-between sm:p-4">
-        <div className="flex w-full flex-col justify-between gap-6 shadow-md sm:flex-row sm:shadow-sm">
-          <NavLink className="pl-4 pt-4 sm:pl-0 sm:pt-0" to="/">
+      <header className="mb-6 flex justify-between bg-background-color-secondary p-4 shadow-md dark:bg-background-color-secondary-dark">
+        <div className="flex w-full flex-col justify-between gap-6 sm:flex-row">
+          <NavLink to="/">
             <img src="/frekkFitLogo.svg" alt="Frekk Fit Party logo" />
           </NavLink>
           {isOpen && (
             <nav
               ref={menuRef}
-              className="flex justify-between rounded-lg bg-background-color-secondary p-6 text-text-primary sm:bg-background-color-primary sm:p-0 dark:bg-background-color-secondary-dark dark:text-text-primary-dark sm:dark:bg-background-color-primary-dark"
+              className="flex justify-between rounded-lg p-6 text-text-primary sm:p-0 dark:text-text-primary-dark"
             >
               {user && (
-                <ul className="mx-auto flex w-full flex-col gap-4 text-center text-text-primary sm:flex-row sm:text-left dark:text-link-primary-dark">
-                  <MainNavLink onClick={toggleMenu} route="/" title="Home" />
+                <ul className="mx-auto flex w-full flex-col gap-4 text-center text-text-primary sm:flex-row sm:items-center sm:text-left dark:text-link-primary-dark">
                   <MainNavLink
-                    onClick={toggleMenu}
+                    onClick={handleNavLinkClick}
+                    route="/"
+                    title="Home"
+                  />
+                  <MainNavLink
+                    onClick={handleNavLinkClick}
                     route="/players"
                     title="Player Insights"
                   />
                   <MainNavLink
-                    onClick={toggleMenu}
+                    onClick={handleNavLinkClick}
                     route="/blog"
                     title="Blog"
                   />
                   <MainNavLink
-                    onClick={toggleMenu}
+                    onClick={handleNavLinkClick}
                     route="/docs"
                     title="Docs"
                   />
                   <div className="mx-auto my-4 w-[80%] border-b border-border-primary sm:hidden dark:border-border-primary-dark"></div>
                   {user.players && (
                     <MainNavLink
-                      onClick={toggleMenu}
+                      onClick={handleNavLinkClick}
                       route="/profile"
                       title="Profile"
                     />
                   )}
-                  <li className="mx-auto sm:ml-auto">
-                    <button
-                      onClick={handleLogout}
-                      className="bg-color-system-error dark:bg-color-system-error-dark rounded px-4 py-2 text-white"
-                    >
-                      Logout
-                    </button>
+                  <li className="mx-auto min-w-fit sm:ml-auto">
+                    <Button onClick={handleLogout} type="secondary">
+                      Log Out
+                    </Button>
                   </li>
                 </ul>
               )}
