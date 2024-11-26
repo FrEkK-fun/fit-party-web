7eb17031871765b3e2c0df76b014c48216a5d9ec
ttowntom <git.running054@passinbox.com>
2024-11-26 11:45:47 +0100

    refactors login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
-import { useLogin } from '../hooks/useLogin';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import useAuthStore from '../store/authStore';
+import { login } from '../utils/http';
+import { saveLocal } from '../utils/localStorage';
 
 import FormInput from '../components/FormInput';
 import Button from '../components/Button';
@@ -9,12 +12,21 @@ import Notification from '../components/Notification';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { login, error, isLoading } = useLogin();
+  const loginUser = useAuthStore((state) => state.login);
+  const navigate = useNavigate();
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      loginUser(data);
+      saveLocal('user', data);
+      navigate('/');
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    await login(email, password);
+    await mutate({ email, password });
   };
 
   return (
@@ -41,12 +53,12 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button disabled={isLoading} type="submit">
+        <Button disabled={isPending} type="submit">
           Log In
         </Button>
-        {error && (
+        {isError && (
           <div className="mt-6">
-            <Notification type="error">{error}</Notification>
+            <Notification type="error">{error.message}</Notification>
           </div>
         )}
       </form>
@@ -61,3 +73,66 @@ const Login = () => {
 };
 
 export default Login;
+
+// import { useState } from 'react';
+// import { useLogin } from '../hooks/useLogin';
+// import { Link } from 'react-router-dom';
+
+// import FormInput from '../components/FormInput';
+// import Button from '../components/Button';
+// import Notification from '../components/Notification';
+
+// const Login = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const { login, error, isLoading } = useLogin();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     await login(email, password);
+//   };
+
+//   return (
+//     <div className="flex w-full flex-col gap-12">
+//       <div className="mx-auto mt-12 text-text-primary dark:text-text-primary-dark">
+//         <h1 className="mb-6 text-center text-5xl font-bold text-text-header dark:text-text-header-dark">
+//           Login
+//         </h1>
+//         <p className="text-center">Access your FrEkK Fit Party account now</p>
+//       </div>
+//       <form
+//         className="mx-auto flex w-full flex-col gap-6 sm:max-w-sm"
+//         onSubmit={handleSubmit}
+//       >
+//         <FormInput
+//           label="Email address"
+//           type="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <FormInput
+//           label="Password"
+//           type="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <Button disabled={isLoading} type="submit">
+//           Log In
+//         </Button>
+//         {error && (
+//           <div className="mt-6">
+//             <Notification type="error">{error}</Notification>
+//           </div>
+//         )}
+//       </form>
+//       <p className="mx-auto text-center text-text-primary dark:text-text-primary-dark">
+//         Don&apos;t have an account?{' '}
+//         <span className="underline hover:no-underline">
+//           <Link to="/signup">Create one today!</Link>
+//         </span>
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Login;
