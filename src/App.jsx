7c6e04c6ece6d06a1302591471758b5fcba661cd
ttowntom <git.running054@passinbox.com>
2024-11-26 14:19:58 +0100

    wip router

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import {
 } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './utils/http';
-import { useAuthContext } from './hooks/useAuthContext';
+import useAuthStore from './store/authStore';
+import { loadLocal } from './utils/localStorage';
 
 /// Pages and components
 import RootLayout from './pages/RootLayout';
@@ -17,28 +18,45 @@ import Player from './pages/Player';
 import Rules from './pages/Rules';
 import Blog from './pages/Blog';
 
-const { user } = useAuthContext();
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'signup', element: !user ? <Signup /> : <Navigate to="/" /> },
-      { path: 'login', element: !user ? <Login /> : <Navigate to="/" /> },
-      { path: 'players', element: !user ? <Login /> : <Players /> },
-      { path: 'players/:playerId', element: !user ? <Login /> : <Player /> },
-      { path: 'rules', element: !user ? <Login /> : <Rules /> },
-      { path: 'blog', element: !user ? <Login /> : <Blog /> },
-    ],
-  },
-]);
-
 function App() {
+  const user = useAuthStore((state) => state.user) || loadLocal('user');
+
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+          { index: true, element: user ? <Home /> : <Navigate to="/login" /> },
+          { path: 'signup', element: <Signup /> },
+          { path: 'login', element: <Login /> },
+          // { path: 'players', element: <Players /> },
+          // { path: 'players/:playerId', element: <Player /> },
+          // { path: 'rules', element: <Rules /> },
+          // { path: 'blog', element: <Blog /> },
+        ],
+      },
+    ],
+    {
+      future: {
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_relativeSplatPath: true,
+        v7_skipActionErrorRevalidation: true,
+        v7_startTransition: true,
+      },
+    }
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <RouterProvider
+        future={{
+          v7_startTransition: true,
+        }}
+        router={router}
+      />
     </QueryClientProvider>
   );
 }
