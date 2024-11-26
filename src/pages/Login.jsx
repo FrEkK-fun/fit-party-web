45e47261f82adcce192cafd289ae204ab44a4142
ttowntom <git.running054@passinbox.com>
2024-11-26 12:57:04 +0100

    uses a generic http post function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import useAuthStore from '../store/authStore';
-import { login } from '../utils/http';
+import { poster } from '../utils/http';
 import { saveLocal } from '../utils/localStorage';
 
 import FormInput from '../components/FormInput';
@@ -16,7 +16,11 @@ const Login = () => {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: login,
+    mutationFn: (body) =>
+      poster({
+        url: '/user/login',
+        body,
+      }),
     onSuccess: (data) => {
       loginUser(data);
       saveLocal('user', data);
@@ -73,66 +77,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// import { useState } from 'react';
-// import { useLogin } from '../hooks/useLogin';
-// import { Link } from 'react-router-dom';
-
-// import FormInput from '../components/FormInput';
-// import Button from '../components/Button';
-// import Notification from '../components/Notification';
-
-// const Login = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const { login, error, isLoading } = useLogin();
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     await login(email, password);
-//   };
-
-//   return (
-//     <div className="flex w-full flex-col gap-12">
-//       <div className="mx-auto mt-12 text-text-primary dark:text-text-primary-dark">
-//         <h1 className="mb-6 text-center text-5xl font-bold text-text-header dark:text-text-header-dark">
-//           Login
-//         </h1>
-//         <p className="text-center">Access your FrEkK Fit Party account now</p>
-//       </div>
-//       <form
-//         className="mx-auto flex w-full flex-col gap-6 sm:max-w-sm"
-//         onSubmit={handleSubmit}
-//       >
-//         <FormInput
-//           label="Email address"
-//           type="email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <FormInput
-//           label="Password"
-//           type="password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <Button disabled={isLoading} type="submit">
-//           Log In
-//         </Button>
-//         {error && (
-//           <div className="mt-6">
-//             <Notification type="error">{error}</Notification>
-//           </div>
-//         )}
-//       </form>
-//       <p className="mx-auto text-center text-text-primary dark:text-text-primary-dark">
-//         Don&apos;t have an account?{' '}
-//         <span className="underline hover:no-underline">
-//           <Link to="/signup">Create one today!</Link>
-//         </span>
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default Login;
--- src/utils/http.js
+++ src/utils/http.js
@@ -2,11 +2,12 @@ import { QueryClient } from '@tanstack/react-query';
 
 import backendURL from '../config';
 
+const baseUrl = `${backendURL}/api`;
 export const queryClient = new QueryClient();
 
-const baseUrl = `${backendURL}/api`;
+export async function fetcher({ queryKey }) {
+  const [url, token] = queryKey;
 
-export async function fetcher(url, token) {
   try {
     const res = await fetch(`${baseUrl}${url}`, {
       headers: {
@@ -22,5 +23,32 @@ export async function fetcher(url, token) {
     return json;
   } catch (error) {
     throw new Error(error.message);
+  }
+}
+
+export async function poster({ url, body }) {
+  try {
+    const response = await fetch(`${baseUrl}${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // Add Authorization header if token exists
+        ...(body.token && {
+          Authorization: `Bearer ${body.token}`,
+        }),
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || 'An error occurred whilse posting data');
+    }
+
+    return data;
+  } catch (error) {
+    // Preserve original error message or fallback
+    throw new Error(error.message || 'Failed to make request');
   }
 }
