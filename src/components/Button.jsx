95bdb92c0accb42299c61e74dc118e067e326d25
ttowntom <git.running054@passinbox.com>
2024-12-02 11:34:16 +0100

    adds full-width option to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({ type, children, ...props }) {
+export default function Button({ type, wFull, children, ...props }) {
   // Base style shared by all button types
-  const baseStyle =
+  let baseStyle =
     'min-w-fit rounded border px-4 py-2 text-text-primary dark:text-text-primary-dark hover:bg-background-color-secondary hover:dark:bg-background-color-secondary-dark';
 
   // Type-specific styles
@@ -10,6 +10,12 @@ export default function Button({ type, children, ...props }) {
     secondary:
       'border-border-tertiary bg-background-color-button-secondary dark:border-border-tertiary-dark dark:bg-background-color-button-secondary-dark dark:hover:border-border-secondary-dark hover:border-border-secondary',
   };
+
+  if (wFull) {
+    baseStyle += ' w-full';
+  } else {
+    baseStyle += ' w-auto';
+  }
 
   // In your component:
   let style = `${baseStyle} ${buttonStyles[type] || buttonStyles.primary}`;
