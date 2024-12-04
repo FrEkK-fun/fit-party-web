56c65e537c321c8691fdb47e6edc8b582269184c
ttowntom <git.running054@passinbox.com>
2024-12-04 10:36:24 +0100

    refactors SectionHeader component to conditionally render content based on available props

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -22,24 +22,29 @@ export default function SectionHeader({
         )}
         <h3 className="mt-2 text-2xl font-bold sm:text-3xl">{title}</h3>
       </div>
-      <div className="pt-10 sm:w-1/2">
-        {text && <p className="mb-6">{text}</p>}
-        {Component && <Component />}
-        {linkText && link && (
-          <Link
-            to={link}
-            className="text-link-primary hover:underline dark:text-link-primary-dark"
-          >
-            {linkText}
-            <span className="">
-              <FontAwesomeIcon
-                icon={byPrefixAndName.fas[`chevron-right`]}
-                className="ml-2 text-sm text-link-secondary dark:text-link-secondary-dark"
-              />
-            </span>
-          </Link>
-        )}
-      </div>
+      {text ||
+        Component ||
+        linkText ||
+        (link && (
+          <div className="pt-10 sm:w-1/2">
+            {text && <p className="mb-6">{text}</p>}
+            {Component && <Component />}
+            {linkText && link && (
+              <Link
+                to={link}
+                className="text-link-primary hover:underline dark:text-link-primary-dark"
+              >
+                {linkText}
+                <span className="">
+                  <FontAwesomeIcon
+                    icon={byPrefixAndName.fas[`chevron-right`]}
+                    className="ml-2 text-sm text-link-secondary dark:text-link-secondary-dark"
+                  />
+                </span>
+              </Link>
+            )}
+          </div>
+        ))}
     </div>
   );
 }
