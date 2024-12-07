0dae80fd3d8eb6b31ed0ec18d41ba25189ac424c
ttowntom <git.running054@passinbox.com>
2024-12-07 11:41:27 +0100

    refactors PlayerQuickStatCard to utilize PlayerGoalAndIcon component for displaying player goals and icons

--- src/components/PlayerQuickStatCard.jsx
+++ src/components/PlayerQuickStatCard.jsx
@@ -5,30 +5,10 @@ import { byPrefixAndName } from '@awesome.me/kit-43505c22f8/icons';
 import { playerWeeklySessions } from '../utils/getPlayerWeeklySessions';
 
 import PlayerClassAndIcon from './PlayerClassAndIcon';
+import PlayerGoalAndIcon from './PlayerGoalAndIcon';
 
 export default function PlayerQuickStatCard({ player }) {
   const weeklySessions = playerWeeklySessions(player);
-
-  let goalDesc;
-  if (player.weekly.goal.description) {
-    goalDesc = player.weekly.goal.description;
-  } else {
-    goalDesc = 'Goal not set!';
-  }
-
-  let goalIcon;
-  let goalIconClass = 'mr-2';
-  if (player.weekly.goal.done) {
-    goalIcon = 'check-circle';
-    goalIconClass += 'text-text-success dark:text-text-success-dark';
-  } else if (
-    !player.weekly.goal.done &&
-    player.weekly.goal.description !== ''
-  ) {
-    goalIcon = 'spinner-scale';
-  } else if (player.weekly.goal.description === '') {
-    goalIcon = 'triangle-exclamation';
-  }
 
   return (
     <Link
@@ -70,13 +50,7 @@ export default function PlayerQuickStatCard({ player }) {
           />
           <p className="min-w-fit">Level {player.weekly.level}</p>
         </div>
-        <div className="flex items-center gap-1 font-medium">
-          <FontAwesomeIcon
-            className={goalIconClass}
-            icon={byPrefixAndName.fad[goalIcon]}
-          />
-          <p className="font-light italic">{goalDesc}</p>
-        </div>
+        <PlayerGoalAndIcon player={player} />
       </div>
     </Link>
   );
