810bec7debd3bad6b0d4de28719312213e6f3378
ttowntom <git.running054@passinbox.com>
2024-12-10 09:34:33 +0100

    refactor: remove unused context providers for authentication, player, and blog

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { AuthContextProvider } from './context/AuthContext';
-import { PlayerContextProvider } from './context/PlayerContext.jsx';
-import { BlogProvider } from './context/BlogContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <AuthContextProvider>
-    <PlayerContextProvider>
-      <BlogProvider>
-        <App />
-      </BlogProvider>
-    </PlayerContextProvider>
-  </AuthContextProvider>
+  <App />
   // </React.StrictMode>
 );
